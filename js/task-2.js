8c0ca64105410a9d1f23859db2e95058438acd14
Sulatn Roziyev <jjo152065@gmail.com>
2023-04-04 15:02:27 +0600

    Сделал 2 задание

---
+++ js/task-2.js
@@ -0,0 +1,7 @@
+const element = document.getElementsByClassName('element');
+console.log(element[0].style.color = 'red');
+console.log(element[1].style.color = 'red');
+console.log(element[2].style.color = 'red');
+console.log(element[3].style.color = 'green');
+console.log(element[4].style.color = 'green');
+console.log(element[5].style.color = 'green');
